81c3d40537942c60d4350c33afb061d6880e5953
Koushik Pramanick <46644710+KOUSHIKDEVSTAR@users.noreply.github.com>
2021-02-05 18:58:47 +0530

    Signed-off-by: Koushik Pramanick <46644710+KOUSHIKDEVSTAR@users.noreply.github.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,11 +8,4 @@ export const environment = {
   SERVER_URL_NEW : 'http://3.25.66.75:3636/api',
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
