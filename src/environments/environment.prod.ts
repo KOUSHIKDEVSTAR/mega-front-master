c421396bc3eb8f62fd14f150106364474d690440
Koushik Pramanick <46644710+KOUSHIKDEVSTAR@users.noreply.github.com>
2021-02-14 00:45:03 +0530

    apply job 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   SERVER_URL: 'https://fast-stream-39179.herokuapp.com/api',
-  BASE_URL:'https://fast-stream-39179.herokuapp.com/images'
+  BASE_URL:'https://fast-stream-39179.herokuapp.com/images',
+  BASE_URL_DOC:'https://fast-stream-39179.herokuapp.com/document'
 };
